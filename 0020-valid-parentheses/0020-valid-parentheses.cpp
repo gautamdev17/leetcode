370554da97db619db06264735d60b00735de1ad9
Gautam <gautamd.nxt@gmail.com>
2026-05-20 23:30:14 +0530

    Time: 3 ms (9.74%), Space: 8.7 MB (88.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> stack;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                stack.push(s[i]);
+            }else{
+                if(!stack.empty()){
+                    char topChar = stack.top();
+                    if ((s[i] == ')' && topChar != '(') || (s[i] == '}' && topChar != '{') || (s[i] == ']' && topChar != '[')) {
+                        return false;
+                    }else{
+                            stack.pop();
+                        }
+                }else{
+                    return 0;
+                }
+            }
+        }
+        return stack.empty();
+    }
+};
