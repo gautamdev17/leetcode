5ffd32c015d70b09245d718f0d92e63f5d71128d
Gautam <gautamd.nxt@gmail.com>
2026-05-21 22:34:04 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,7 +3,16 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maxsum = nums[0];
         int currsum = 0;
-        // so i missed the case where the nums[i] is greater than max itself
+        /* algorithm:
+            1.check if continuing the current subarray is worth it or not:
+                condition: array[i]>currsum+array[i]
+                        then just update currsum = array[i]
+            2.else just keep continuing the current subarray
+
+            update maxsum if highest of currsum history is reached
+
+            easy algorithm!
+        */
         for(int i=0;i<nums.size();i++){
             if(nums[i]>currsum+nums[i]){
                 currsum = nums[i];
