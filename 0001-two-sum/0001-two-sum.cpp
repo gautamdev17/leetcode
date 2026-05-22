38b74161ea6f2178f47ff0e21ee73afb0ca99d67
Gautam <gautamd.nxt@gmail.com>
2026-05-23 00:59:28 +0530

    Time: 3 ms (66.44%), Space: 14.9 MB (36.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    //using two pointer technique for most efficient solution
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j] == target){
-                    return {i,j};
-                }
-            }
+        //to prevent loss of correct indice of value
+        vector<pair<int, int>> numindex;
+        for (int i = 0; i < nums.size(); i++) {
+            numindex.push_back({nums[i], i});
+        }
+        sort(numindex.begin(), numindex.end());
+        int i=0;
+        int j=numindex.size()-1;
+        while(i!=j){
+            if(numindex[i].first+numindex[j].first>target)
+                j--;
+            else if(numindex[i].first+numindex[j].first<target)
+                i++;
+            else
+                return {numindex[i].second,numindex[j].second};
         }
         return {};
     }
