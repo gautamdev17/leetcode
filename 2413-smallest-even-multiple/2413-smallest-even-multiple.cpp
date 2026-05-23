13130ed8b1fd9457132134632f53905f152f00ac
Gautam <gautamd.nxt@gmail.com>
2026-05-23 14:54:06 +0530

    Time: 0 ms (100%), Space: 7.8 MB (94.22%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        return (n%2)*2*n+((n+1)%2)*n;
+        //first submission: return (n%2)*2*n+((n+1)%2)*n;
+        return n<<(n&1);// (n&1) is 1, if n is odd, then it multiplies n by 2, or returns n itself
     }
 };
