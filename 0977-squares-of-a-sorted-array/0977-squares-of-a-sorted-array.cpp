278e87c9ce7cd3a8318397ddebdcf4f8535e50b5
Gautam <gautamd.nxt@gmail.com>
2026-05-25 21:44:16 +0530

    Time: 2 ms (53.61%), Space: 30.2 MB (86.99%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -4,16 +4,13 @@ public:
         std::vector<int> result(nums.size());
         int i=0;
         int j=nums.size()-1;
-        for(int k=0;k<nums.size();k++){
-            nums[k]=nums[k]*nums[k];
-        }
         int k = nums.size()-1;
         while(i<=j){
-            if(nums[i]<=nums[j]){// handle equal case anywhere its symmetrical
-                result[k]=nums[j];
+            if(nums[i]*nums[i]<=nums[j]*nums[j]){// handle equal case anywhere its symmetrical
+                result[k]=nums[j]*nums[j];
                 j--;
             }else{
-                result[k]=nums[i];
+                result[k]=nums[i]*nums[i];
                 i++;
             }
             k--;
